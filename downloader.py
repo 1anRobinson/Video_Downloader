6ac7629b9388b2dec380d877df142bbc0e953602
DerekYeeter <alex.robi.2020@gmail.com>
2022-06-24 03:30:28 -0400

    should now download onto the desktop of the user but not the OneDrive desktop which is weird

--- downloader.py
+++ downloader.py
@@ -1,7 +1,23 @@
-from pytube import YouTube
+"""
+6/24/2022
 
+@author: Ian Robinson
+"""
+from pytube import YouTube
+import os
+
+print(os.path.expanduser('~/Desktop'))
 url = input('Paste URL here: ') #get the link as input from user
-video = YouTube(url) # variable of ty
+while "www.youtube.com/watch?v=" not in url: # keep looping if url is invalid
+    url = input('Invalid URL. Try Again or press \"E\" to exit: ')
+    if url == 'E':
+        exit()
+    
+video = YouTube(url) # store the url of the type YouTube into variable
 
 print('\n*****************Video Title*****************')
 print(video.title) #get and print Youtube Title
+
+video = video.streams.get_highest_resolution() #set stream resolution
+
+video.download(os.path.expanduser('~/Desktop')) #download method that downloads to the desktop
