c97ea3f125f5b87c278b8bea614a27ac8594b3ec
DerekYeeter <alex.robi.2020@gmail.com>
2022-09-16 16:37:38 -0400

    made the downloader more user friendly. Still hangs at the end :(

--- downloader.py
+++ downloader.py
@@ -4,9 +4,10 @@
 @author: Ian Robinson
 """
 from pytube import YouTube
-import os
+#import os
 
-print(os.path.expanduser('~/Desktop'))
+print('Welcome to the video downloader by Ian Robinson!')
+path = input('Please specify the desintation of your download: ') # path to directory they want to store video
 url = input('Paste URL here: ') #get the link as input from user
 while "www.youtube.com/watch?v=" not in url: # keep looping if url is invalid
     url = input('Invalid URL. Try Again or press \"E\" to exit: ')
@@ -15,9 +16,13 @@ while "www.youtube.com/watch?v=" not in url: # keep looping if url is invalid
     
 video = YouTube(url) # store the url of the type YouTube into variable
 
-print('\n*****************Video Title*****************')
+print('\n*****************Video***********************')
 print(video.title) #get and print Youtube Title
-
-video = video.streams.get_highest_resolution() #set stream resolution
-
-video.download(os.path.expanduser('~/Desktop')) #download method that downloads to the desktop
+print('Size: ', video.streams.get_highest_resolution().filesize,'MB')
+response = input('Continue with download? [Y/N]: ')
+if response == 'y' or response == 'Y':
+    video = video.streams.get_highest_resolution() #set stream resolution
+    video.download(path)
+elif response == 'n' or response == 'N':
+    print('Roger. Thank you for using the video downloader!')
+    exit()
